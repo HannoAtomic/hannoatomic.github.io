ed358ec006719e217f4cb7cfafc876500055a078
HannoAtomic <theepicest2@gmail.com>
2024-01-21 15:57:19 +0000

    added side swap and buttons disabling depending on side.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+//track the consecutive losses
 var lostConsec = 1;
+//side = 0 is CT, 1 for T
+var side = 0;
 
 function changeMoney(amount) {
 	const money = document.getElementById('money').textContent;
 	let raw_value = money.slice(1);
 	raw_value = parseInt(raw_value) + amount;
+    //consecutive losses cannot be negative
+    if(amount > 0 && lostConsec > 0){
+        lostConsec = lostConsec -1;
+    }
 	document.getElementById('money').textContent = "$" + raw_value;
 	console.log("Value Changed")
 }
@@ -31,14 +38,17 @@ function loss(bombPlant){
 			changeMoney(3400 + bonus);
 			break;
 	}
+    //consecutively losses cap at 4
+    if(lostConsec < 4){
+        lostConsec = lostConsec + 1;
+    } else {
+        lostConsec = 4;
+    }
 	console.log("Loss Registered")
 }
 
 function force(){
-	const money = document.getElementById('money').textContent;
-	let raw_value = money.slice(1);
-	if parseInt(raw_value)
-	console.log("Force Registered")	
+	document.getElementById('money').textContent = "$150";
 }
 
 function reset(){
@@ -51,6 +61,23 @@ function reset(){
 function changeSide(){
 	//remove bomb addition
 	//add alive counter
+    //swap side coin
+    console.log(document.getElementById('sideImg').src)
+    if(document.getElementById('sideImg').src.indexOf( "images/ct.png" ) > 0){
+        document.getElementById('sideImg').src = "images/T.png";
+        document.getElementById('sideImg').alt = "T";
+        console.log("CT detected, Swapping to T")
+        side = 0;
+        document.getElementById("lossWithPlant").disabled = false;
+        document.getElementById("winCondition").textContent = "Win by Explode";
+    } else {
+        document.getElementById('sideImg').src = "images/ct.png";
+        document.getElementById('sideImg').alt = "CT";
+        console.log("T detected, Swapping to CT")
+        side = 1;
+        document.getElementById("lossWithPlant").disabled = true;;
+        document.getElementById("winCondition").textContent = "Win by Defuse";
+    }
 	reset()
 	console.log("SIDEFLIP RESET")
 }
