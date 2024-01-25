d9056799accbe3144d002c8943b9c15942982150
HannoAtomic <theepicest2@gmail.com>
2024-01-25 01:51:34 +0000

    added university faceit elo tracking functionality

---
+++ unitracker.js
@@ -0,0 +1,40 @@
+//const playerNames = document.getElementById('playerNames').textContent;
+const playerNameList = document.getElementById("nameList");
+const playerEloList = document.getElementById("eloList");
+
+
+function search(){
+    fetch("http://127.0.0.1:5000/faceitAPIfetch", {
+        method: "GET"
+    }).then((Response => Response.json())).then((json) => replace(json))
+}
+
+var playerNamesList = [];
+var playerElosList = [];
+
+function replace(text){
+    console.log("replace text")
+    while(playerEloList.firstChild){
+        playerEloList.innerHTML = "";
+        playerNameList.innerHTML = "";
+        var playerNamesList = [];
+        var playerElosList = [];
+    }
+    
+    text.forEach((item) => {
+        playerNamesList.push(item[0]);
+        playerElosList.push(item[1]);
+      });
+    playerNamesList.forEach((name) => {
+        var listElement = document.createElement("li");
+        listElement.appendChild(document.createTextNode(name));
+        playerNameList.appendChild(listElement);
+    })
+
+    playerElosList.forEach((elo) => {
+        var listElement = document.createElement("li");
+        listElement.appendChild(document.createTextNode(elo));
+        playerEloList.appendChild(listElement);
+    })
+
+}
